513313e8147d6fc66d249d2adeaafb829f61eb2a
Sachin <sachsaini2004@gmail.com>
2025-07-28 22:20:50 +0530

    refactor: remove outdated comment regarding PDFTemplate data fetching

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -26,7 +26,6 @@ const Preview = ({ formData }) => {
       {/* PDF Preview */}
       <div className="max-w-4xl mx-auto p-4">
         <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-          {/* The PDFTemplate now receives the fetched data */}
           <PDFTemplate formData={formData} />
         </div>
       </div>
